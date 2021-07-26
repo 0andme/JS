818419b4517733671ef9f88a40f88240bb480f5a
0andme <83719746+0andme@users.noreply.github.com>
2021-07-26 16:36:55 +0900

    Add: add 2 js files 1. " for of " 문법 실습 2." for of " 문법 실습

---
+++ 19_for-of_for-in/for-of.js
@@ -0,0 +1,8 @@
+const numbers = [10, 20, 30, 40, 50];
+console.clear();
+for (let number of numbers) {
+  console.log(number);
+}
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
