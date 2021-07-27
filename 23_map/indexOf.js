cefae4a4060f81a4a2689bdfd03ac5bd207b6297
0andme <83719746+0andme@users.noreply.github.com>
2021-07-27 20:54:39 +0900

    Feat : add indexOf indexOf 실습 코드 변경

--- 23_map/indexOf.js
+++ 23_map/indexOf.js
@@ -1,3 +1,5 @@
-const superHeroes = ["토르", "블랙위도우", "아이언맨"];
-const index = superHeroes.indexOf("토르");
-console.log(index);
+const arrayA = ["ABC", 1, "감자"];
+const indexA = arrayA.indexOf("감자");
+const indexB = arrayA.indexOf(1);
+console.log(indexA);
+console.log(indexB);
