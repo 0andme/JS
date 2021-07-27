b7c68d0031fd51ad693353607f2ecb7481f914cd
0andme <83719746+0andme@users.noreply.github.com>
2021-07-27 16:37:56 +0900

    Feat : about array 배열 선언 실습

--- 16_array/array.js
+++ 16_array/array.js
@@ -1,5 +1,7 @@
-//const array = [1, 2, 3, 4, 5];
+const array = [1, 2, 3, 4, 5];
+const strArraay = ["감자", "고구마"];
 const objects = [{ name: "감자" }, { name: "고구마" }];
+const exArray = [2, "문자열", { name: "객체" }];
 console.log(objects);
 console.log(objects[0]);
 console.log(objects[1]);
