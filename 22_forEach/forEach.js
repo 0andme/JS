cb45d9c65b7163335cc140c4200f0b57643836a8
0andme <83719746+0andme@users.noreply.github.com>
2021-07-27 20:00:25 +0900

    Feat : about forEach forEach 메서드 실습 추가

--- 22_forEach/forEach.js
+++ 22_forEach/forEach.js
@@ -1,20 +1,38 @@
-const superHeroes = ["캡틴", "토르", "블랙위도우"];
+const users = ["철이", "미애", "길동"];
 
-for (let i = 0; i < superHeroes.length; i++) {
-  console.log(superHeroes[i]);
+/*1. for 문 이용
+
+for (let i = 0; i < users.length; i++) {
+  console.log(users[i]);
 }
+/*
 
-function print(hero) {
-  console.log(hero);
+/*2. function과 for 문을 이용
+function print(user) {
+  console.log(user);
 }
+for (let i = 0; i < users.length; i++) {
+  print(users[i]);
+}*/
+/* 화살표 함수와 for 문을 이용
+const print = (user) => console.log(user);
+for (let i = 0; i < users.length; i++) {
+  print(users[i]);
+}
+*/
+/*forEach 1. 파라미터에 함수명
+function print(userName) {
+  console.log(userName);
+}
+users.forEach(print);
+*/
 
-/*함수호출*/
-superHeroes.forEach(print);
-/*함수 바로 작성*/
-superHeroes.forEach(function (hero) {
-  console.log(hero);
+// /*함수 바로 작성*/
+users.forEach(function (userName) {
+  console.log(userName);
 });
+
 /*화살표 함수로 작성*/
-superHeroes.forEach((hero) => {
-  console.log(hero);
+users.forEach((userName) => {
+  console.log(userName);
 });
