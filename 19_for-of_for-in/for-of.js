b6986b7c5a3c68ec9205cfb1f6fcb9a778a68b48
0andme <83719746+0andme@users.noreply.github.com>
2021-07-27 17:33:43 +0900

    Feat : add code for-of 문에 대한 예제 추가

--- 19_for-of_for-in/for-of.js
+++ 19_for-of_for-in/for-of.js
@@ -1,8 +1,17 @@
 const numbers = [10, 20, 30, 40, 50];
-console.clear();
 for (let number of numbers) {
   console.log(number);
 }
 for (let i = 0; i < numbers.length; i++) {
   console.log(numbers[i]);
+}
+const user = {
+  name: "홍길동",
+  age: 10
+};
+for (let value of user) {
+  console.log(value);
+}
+for (let c of "hello") {
+  console.log(c);
 }
