d18060bfadf6db9764a334b62d5b80289eab0aa2
0andme <83719746+0andme@users.noreply.github.com>
2021-08-03 21:40:12 +0900

    feat : add concat file concat 실습

--- 26/unshift.js
+++ 26/unshift.js
@@ -3,3 +3,10 @@ const numbers = [1, 2, 3, 4, 5];
 
 numbers.unshift(6);
 console.log(numbers);
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const concated1 = arr1.concat(arr2);
+const concated2 = arr2.concat(arr1);
+console.log(concated1);
+console.log(concated2);
