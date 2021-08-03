1b4cab76eaf6f5df1a8bd73ba5e6c0a1bb68f274
0andme <83719746+0andme@users.noreply.github.com>
2021-08-03 21:44:25 +0900

    feat : update files 배열 내장함수 실습 파일 업데이트

---
+++ 26/concat.js
@@ -0,0 +1,9 @@
+// 두 배열의 합치는 메서드
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const concated1 = arr1.concat(arr2);
+const concated2 = arr2.concat(arr1);
+console.log(concated1);
+console.log(concated2);
